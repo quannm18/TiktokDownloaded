e8f2d88982153d23b9eabda18c20db6ec55bab61
thinhkaku <thinhkaku.a3k51@gmail.com>
2022-06-14 16:05:06 +0700

    First

---
+++ app/src/main/java/com/example/tiktokdownloaded/model/TikTokEntity.kt
@@ -0,0 +1,31 @@
+package com.example.tiktokdownloaded.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tiktok_table")
+data class TikTokEntity (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val urlVideo: String,
+    val urlMusic: String,
+    val urlThumbnail: String,
+    val author: String,
+    val duration: String,
+    val date: String,
+    )
+
+
+fun convertTikTok(tikTokModel: TikTokModel, date: String): TikTokEntity {
+    return TikTokEntity(
+        id = 0,
+        title = tikTokModel.awemeDetail!!.desc,
+        urlVideo = tikTokModel.awemeDetail.video.play_addr.url_list[2],
+        urlMusic = tikTokModel.awemeDetail.music.playUrlMusic.uri,
+        urlThumbnail = tikTokModel.awemeDetail.video.origin_cover.url_list[0],
+        author = tikTokModel.awemeDetail.author.nickname,
+        duration = tikTokModel.awemeDetail.video.duration,
+        date = date
+    )
+}
---
+++ app/src/main/java/com/example/tiktokdownloaded/util/TikTokDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.tiktokdownloaded.util
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.tiktokdownloaded.dao.TikTokDAO
+import com.example.tiktokdownloaded.model.TikTokEntity
+
+@Database(entities = [TikTokEntity::class], version = 1, exportSchema = false)
+abstract class TikTokDatabase : RoomDatabase() {
+    abstract fun tikTokDAO(): TikTokDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE : TikTokDatabase ? =null
+
+        fun getDatabase(context: Context): TikTokDatabase{
+            var tempInstance = INSTANCE
+
+            if (tempInstance!=null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TikTokDatabase::class.java,
+                    "tiktok_database"
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
